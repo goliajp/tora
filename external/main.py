4c3cc2110122b8c747088f20163dc492df66226d
tora <ws@golia.jp>
2024-06-10 15:54:56 +0900

    ci: zip test

--- external/main.py
+++ external/main.py
@@ -7,6 +7,7 @@ import asyncio
 import json
 import math
 import random
+import requests
 
 app = FastAPI()
 
@@ -37,31 +38,45 @@ async def websocket_endpoint(websocket: WebSocket):
 #     "url": "http://127.0.0.1:8000/download/platform/1.0.1"
 # }
 
-latest_version = "1.0.1"
+
 url = "https://cdn.golia.jp/downloads/electron-tora"
 
-@app.get("/update/{platform}//{version}")
-async def check_for_updates(platform: str, version: str):
-    # 检查客户端版本是否低于服务器的最新版本
-    if version < latest_version:
-        # 如果需要更新，返回更新详情
-        return {
-            "url": f"http://127.0.0.1:8000/download/{platform}/{latest_version}",
-            "name": latest_version,
-            # 可以根据实际需要添加其他字段，比如更新的说明、大小等
-        }
+
+@app.get("/update/{platform}/{arch}/{version}")
+async def check_for_updates(platform: str, version: str, arch: str):
+
+    # latest_version取自https://cdn.golia.jp/downloads/electron-tora/darwin_version.json的darwin_version字段
+
+    json_url = f"{url}/{platform}_version.json"
+    response = requests.get(json_url)
+    if response.status_code == 200:
+        latest_version = response.json()[f"{platform}_version"]
+        # 检查客户端版本是否低于服务器的最新版本
+        if version < latest_version:
+
+            # 如果需要更新，返回更新详情
+            return {
+                "url": f"/download/{platform}/{arch}/{latest_version}",
+                "name": latest_version,
+                # 可以根据实际需要添加其他字段，比如更新的说明、大小等
+            }
+        else:
+            # 如果不需要更新，通过状态码 204 表明没有内容响应
+            raise HTTPException(status_code=204)
     else:
         # 如果不需要更新，通过状态码 204 表明没有内容响应
         raise HTTPException(status_code=204)
 
 
 # 对于下载链接, 使用了路由路径参数来动态获取版本号
-@app.get("/download/{platform}/{version}")
-async def download_file(platform: str, version: str):
+@app.get("/download/{platform}/{arch}/{version}")
+async def download_file(platform: str, version: str, arch: str):
     # 下面我们构造了文件的完整路径
     print(platform)
+    # 如果platform是linux，返回AppImage格式的文件，如果是macOS，返回dmg格式的文件，如果是windows，返回msi格式的文件
+    # suffix = "AppImage" if platform == "linux" else "dmg" if platform == "macOS" else "msi"
     # 根据不同的 platform 返回不同的文件
-    file_path = f"downloads/electron-tora-{version}-arm64-mac.zip"
+    file_path = f"{url}/electron-tora-{platform}-{latest_version}-{arch}.zip",
     filename = f"electron-tora-{version}-arm64-mac.zip"
 
     print(file_path)
